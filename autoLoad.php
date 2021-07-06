dbf56dd7165e41b1565c24a474a12772e426541f
leightonvanrooijen <leighton.vanrooijen@ezyvet.com>
2021-07-06 21:44:07 +1200

    added classes folder

--- autoLoad.php
+++ autoLoad.php
@@ -3,7 +3,7 @@
 /** Loads all classes in the root directory */
 function __autoload($class_name)
 {
-    if (file_exists('./' . $class_name . '.php')) {
-        require_once './' . $class_name . '.php';
+    if (file_exists('./classes/' . $class_name . '.php')) {
+        require_once './classes/' . $class_name . '.php';
     }
 }
